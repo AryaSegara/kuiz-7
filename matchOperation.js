00020a83b71e84b0c0e41ef659a88c1491a73af1
Arya Segara <aryasegara10@gmail.com>
2023-10-17 20:35:21 +0700

    kuizz

---
+++ matchOperation.js
@@ -0,0 +1,11 @@
+export function tambah(a,b){
+    return "Hasil Penjumlahan : " + a + b;
+}
+
+export function kali (c,d){
+    return "hasil perkalian : " + c * d;
+}
+
+export function phi(e){
+    return "nilai PiI : " + e;
+}
